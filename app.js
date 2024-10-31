3182399808a036e4f64c7a09659745b74a403b6e
husnain3964 <husnain3964@gmail.com>
2024-10-31 07:23:34 -0700

    add some changes

--- app.js
+++ app.js
@@ -1,48 +1,94 @@
+let turn = "0";
 
-let turn = "x"
+//function for change turn
 
-const box= document.getElementsByClassName('box')
-function  hello() {
- 
-if (turn=="0"){
-    turn="x"
-}else{
-    turn='0'
+const changeTurn = () => {
+  if (turn === "X") {
+    turn = "0";
+  } else {
+    turn = "X";
+  }
+  // return turn=== 'X' ?'0': 'X'
+};
+
+// check win
+
+let checkWin = () => {
+    let boxTexts = document.getElementsByClassName("check"); 
+    let won = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    won.forEach((e) => {
+        if (
+            boxTexts[e[0]].innerText === boxTexts[e[1]].innerText &&
+            boxTexts[e[1]].innerText === boxTexts[e[2]].innerText && 
+            boxTexts[e[0]].innerText !== ""
+        ) {
+            document.querySelector('#h11').innerText = boxTexts[e[0]].innerText + ' won!'; // Fixed the assignment
+            document.querySelector('#h22').innerText = ''; // Clear the turn message on win
+            // Optionally disable further clicks or reset the game here
+        }
+    });
+
+    let win = document.querySelector("#h22");
+    win.innerText = `${turn}'s turn`; // Correctly set the turn message
+};
+
+// let checkWin = () => {
+//   let boxTexts = document.getElementsByClassName("check");
+//   let won = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   won.forEach((e) => {
+//     if (
+//       boxTexts[e[0]].innerText === boxTexts[e[1]].innerText &&
+//       boxTexts[e[1]] === boxTexts[e[2]].innerText &&
+//       boxTexts[e[0]].innerText !== ""
+//     ) { 
+//         document.querySelector('#h11').innerText = boxTexts[e[0]].innerText + 'won'  ;
+//     }
+//   });
+
+//   let win = document.querySelector("#h22");
+//   win.innerHTML = `${turn}'s turn `;
+// };
+
+// game logic
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((box) => {
+  let boxText = box.querySelector(".check");
+  box.addEventListener("click", () => {
+    if (boxText.innerText === "") {
+      boxText.innerText = turn;
+      changeTurn();
+      checkWin();
+    }
+      });
+});
+
+
+function restart(){
+   let blank = document.getElementsByClassName('check')
+   Array.from(blank).forEach(e=>{
+    e.innerText=''
+
+   })
+   document.getElementById('h11').innerHTML='start new game'
+
 }
-
-function changeTurn(turn) {
-  const headinggg= document.getElementById("h22")
- headinggg.textContent=`turn is ${turn}`
-}
-changeTurn(turn)
-console.log(turn);
-
-}
-
-
-function mainLogic(){
-    const newArr= Array.from(box)
-console.log(newArr);
-}
-mainLogic()
-
-
-
-
-
-
-
-
-
-
-
-function colooor() {
-    let col = document.getElementById('restart');
-     col.addEventListener("click" ,() => {
-        col.style.Color = "red"
-        console.log('hello');
-    })
-    
-}
-colooor()
-
